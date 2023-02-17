e7c8664bbbc8e3e1b98ca021aea009e480b2497c
arthursmiles <arthurmdala@gmail.com>
2023-02-16 17:45:23 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+<<<<<<< HEAD
+ * main - prints the various size of various types
+ * Description; prints the size of various type
+ * return: 0 if exited properly, non-zero otherwise
+=======
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+>>>>>>> 3700864f05495290bb5ac3b2b832e97eac7bf52e
+ */
+int main(void)
+{
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
+<<<<<<< HEAD
+	 return (0);
+=======
+return (0);
+>>>>>>> 3700864f05495290bb5ac3b2b832e97eac7bf52e
+}
