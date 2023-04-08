6c6e03b42f1f1c22daf3c12b71a6396fedb7a4e4
arthursmiles <arthurmadala@gmail.com>
2023-04-08 04:05:36 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,16 +5,16 @@
  * @b: A pointer to binary string
  * Return: unsigned integer converted number
  */
-
 unsigned int binary_to_uint(const char *b)
-
 {
 	unsigned int base = 1, bin = 0, len = 0;
 
 	if (b == NULL)
 		return (0);
+
 	while (b[len])
 		len++;
+
 	while (len)
 	{
 		if (b[len - 1] != '0' && b[len - 1] != '1')
@@ -23,7 +23,7 @@ unsigned int binary_to_uint(const char *b)
 		if (b[len - 1] == '1')
 			bin += base;
 		base *= 2;
-		len--;	
+		len--;
 	}
 	return (bin);
 }
